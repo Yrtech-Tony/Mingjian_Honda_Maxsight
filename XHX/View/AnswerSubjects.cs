813afe47d730880d0b5bf96d89ce39a5c386865f
Tony <moujunsheng15@163.com>
2018-03-05 23:34:31 +0800

    删除无用文件

--- XHX/View/AnswerSubjects.cs
+++ XHX/View/AnswerSubjects.cs
@@ -1340,6 +1340,11 @@ namespace XHX.View
             string strList = "";
 
             DataSet ds = service.GetNotAnswerSubject(ProjectCode_Golbal, ShopCode_Golbal);
+            if (ds.Tables.Count == 0)
+            {
+                CommonHandler.ShowMessage(MessageType.Information, "没事有设置试卷类型");
+                return;
+            }
             if (ds.Tables[0].Rows.Count > 0)
             {
                 for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
